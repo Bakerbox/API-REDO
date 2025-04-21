a705786d1027b1cbec32083686284ecb69a19af2
Bakerbox <bekamado108@yahoo.com>
2025-04-21 16:57:37 +0200

    TODO finished
    I already handle errors with tracelog here

--- Source/main.cpp
+++ Source/main.cpp
@@ -25,9 +25,8 @@ int main()
         
     }
     catch (const std::exception& e) {
-        // Handle any exceptions that might be thrown
         TraceLog(LOG_ERROR, "Exception caught: %s", e.what());
-        //TODO: consider using std::cerr for exceptional / error outputs, but tracelog probably does something similar
+        
         CloseWindow();        
     }
     return EXIT_SUCCESS;
